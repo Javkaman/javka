6853d9f886748f67669bc347d092ec0f5fd45b67
Javkaman2 <goltrix@o2.pl>
2019-11-03 19:08:02 +0100

    Commituje pliki

--- src/Cap/NumeroUno.java
+++ src/Cap/NumeroUno.java
@@ -1,4 +1,13 @@
 package Cap;
 
 public class NumeroUno {
+    public static void main(String[] args){
+        boolean big = isBig(101);
+
+    }
+
+    static boolean isBig(int someNumber) {
+        return someNumber > 100;
+    }
 }
+
